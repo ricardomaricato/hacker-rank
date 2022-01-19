9fa26df8591e809c2415d663fc548f20d246fd06
Ricardo Maricato <ricardo.maricato@hotmail.com>
2022-01-19 20:15:20 -0300

    refactor: JavaList

--- src/dataStructures/JavaList.java
+++ src/dataStructures/JavaList.java
@@ -8,14 +8,12 @@ public class JavaList {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
-
 		if (n < 1 || n > 4000) {
 			System.out.println("Input not allowed!");
 			return;
 		}
 
 		List<Integer> list = new ArrayList<>(n);
-
 		for (int i = 0; i < n; i++) {
 			int num = in.nextInt();
 			list.add(i, num);
@@ -23,13 +21,12 @@ public class JavaList {
 
 		int queries = in.nextInt();
 		if (queries < 1 || queries > 4000) {
-			System.out.println("Input not allowed!");
+			System.out.println("Input queries not allowed!");
 			return;
 		}
 
 		for (int i = 0; i < queries; i++) {
 			String method = in.next();
-
 			if (method.equalsIgnoreCase("insert")) {
 				int x = in.nextInt();
 				int y = in.nextInt();
