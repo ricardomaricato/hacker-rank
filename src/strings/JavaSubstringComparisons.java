b9d2e2b0d725bc3c481b6917b975c2492a896254
Ricardo Maricato <ricardo.maricato@hotmail.com>
2022-02-01 21:42:25 -0300

    challenge: JavaSubstringComparisons

--- src/strings/JavaSubstringComparisons.java
+++ src/strings/JavaSubstringComparisons.java
@@ -5,25 +5,34 @@ import java.util.Scanner;
 
 public class JavaSubstringComparisons {
 	public static void main(String[] args) {
-//		Scanner scan = new Scanner(System.in);
-//		String s = scan.next();
-//		int k = scan.nextInt();
-//		scan.close();
-//
-//		System.out.println(getSmallestAndLargest(s, k));
-		String test = "welcometojava";
+		Scanner scan = new Scanner(System.in);
+		String s = scan.next();
+		int k = scan.nextInt();
+		scan.close();
 
-		System.out.println(Arrays.toString(test.split("(?<=\\G.{3})")));
-
+		System.out.println(getSmallestAndLargest(s, k));
 	}
 
 	public static String getSmallestAndLargest(String s, int k) {
 		String smallest = "";
 		String largest = "";
 
-		// Complete the function
-		// 'smallest' must be the lexicographically smallest substring of length 'k'
-		// 'largest' must be the lexicographically largest substring of length 'k'
+		if (s.length() < 1 || s.length() > 1000) {
+			System.out.println("Input not allowed!");
+			return "";
+		}
+
+		int arraySize = s.length() - k + 1;
+		String[] substringsToComparisons = new String[arraySize];
+
+		for (int i = 0; i < arraySize; i++) {
+			substringsToComparisons[i] = s.substring(i, i + k);
+		}
+
+		java.util.Arrays.sort(substringsToComparisons);
+
+		smallest = substringsToComparisons[0];
+		largest = substringsToComparisons[arraySize - 1];
 
 		return smallest + "\n" + largest;
 	}
