e849f8c584e8306fa9180ed0259bd3e3ad113617
Ricardo Maricato <ricardo.maricato@hotmail.com>
2022-02-02 14:05:43 -0300

    challenge: JavaPatternSyntaxChecker

--- src/strings/JavaPatternSyntaxChecker.java
+++ src/strings/JavaPatternSyntaxChecker.java
@@ -1,11 +1,10 @@
 package strings;
 
 import java.util.Scanner;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
-public class PatternSyntaxChecker {
+public class JavaPatternSyntaxChecker {
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
 		int testCases = Integer.parseInt(in.nextLine());
