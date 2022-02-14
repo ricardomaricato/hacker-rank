61673b1c9c573b037c4fb873cebc97a9e7a85853
Ricardo Maricato <ricardo.maricato_dextra@prestador.globo>
2022-02-14 20:03:39 -0300

    challenge: Java Date and Time

---
+++ src/introduction/JavaDateAndTime.java
@@ -0,0 +1,54 @@
+package introduction;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+class Result {
+
+	/*
+	 * Complete the 'findDay' function below.
+	 *
+	 * The function is expected to return a STRING.
+	 * The function accepts following parameters:
+	 *  1. INTEGER month
+	 *  2. INTEGER day
+	 *  3. INTEGER year
+	 */
+
+	public static String findDay(int month, int day, int year) {
+		if (year <= 2000 || year >= 3000) {
+			System.out.println("Input not allowed!");
+		}
+
+		Calendar cal = Calendar.getInstance();
+		cal.set(year, month - 1, day);
+
+		SimpleDateFormat format = new SimpleDateFormat("EEEE");
+		return format.format(cal.getTime()).toUpperCase();
+	}
+
+}
+
+public class JavaDateAndTime {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+		String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+		int month = Integer.parseInt(firstMultipleInput[0]);
+
+		int day = Integer.parseInt(firstMultipleInput[1]);
+
+		int year = Integer.parseInt(firstMultipleInput[2]);
+
+		String res = Result.findDay(month, day, year);
+
+		bufferedWriter.write(res);
+		bufferedWriter.newLine();
+
+		bufferedReader.close();
+		bufferedWriter.close();
+	}
+}
