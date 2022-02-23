f2341d8fe3abfd98baf4d243f7136ee1f8ba3800
Ricardo Maricato <ricardo.maricato@hotmail.com>
2022-02-23 11:28:57 -0300

    challenge: Java String Tokens

---
+++ src/strings/JavaStringTokens.java
@@ -0,0 +1,22 @@
+package strings;
+
+import java.util.Scanner;
+
+public class JavaStringTokens {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String s = scan.nextLine().trim();
+		if (s.length() == 0 || s.length() > 400000) {
+			System.out.println("0");
+			return;
+		}
+
+		String[] strings = s.split("\\W+|_");
+		System.out.println(strings.length);
+		for (String string : strings) {
+			System.out.println(string);
+		}
+		scan.close();
+	}
+}
+
